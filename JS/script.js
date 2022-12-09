dcb7bfedfcd637dfecff1f6936d3456dda11421e
BigbrotherIG <itzbigbroda@gmail.com>
2022-12-09 15:33:37 +0100

    WebBuilder first release.

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+
+//  const headerScroll = document.querySelector(".main-header");
+
+
+
+// //  Add EventListener to headerScroll
+
+// headerScroll.addEventListener("click", scrol)
+
+// function scrol () {
+    
+// }
